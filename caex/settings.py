a60c2e4e60cccf2e9b9b9bf383e627567cd6a6e5
Uginius <e.slastin@gmail.com>
2024-02-04 11:41:25 +0300

    main page arrows added

--- caex/settings.py
+++ caex/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = reverse_lazy('main:about-me')
+LOGIN_REDIRECT_URL = reverse_lazy('about-me')
