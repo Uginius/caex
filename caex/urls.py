e8cde55e846a47ac3efbe7186e1ebd884b14135b
HardRope <tanabist@gmail.com>
2024-02-07 20:53:03 +0300

    fix static

--- caex/urls.py
+++ caex/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,6 +29,7 @@ urlpatterns = [
     path('easy/', include('easycrypto.urls'), name='easy'),
     path('contacts/', include('contacts.urls'), name='contacts'),
 ]
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
