c0fa65f20e2caf55fc58b26b5298e0e3d9a50508
HardRope <tanabist@gmail.com>
2024-02-08 14:38:18 +0300

    correct static urls

--- caex/settings.py
+++ caex/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets")
+    os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
