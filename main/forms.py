3d5c1425d52f3afd79e6757865dbd3495731d695
Uginius <e.slastin@gmail.com>
2024-03-01 10:45:06 +0300

    design update initial

--- main/forms.py
+++ main/forms.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Passw confirmation', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     crypto_exchange = forms.BooleanField(required=False)
     foreign_exchange = forms.BooleanField(required=False)
