7c604b1e8703306ff07fb3e3b203f824c4faa0df
HardRope <tanabist@gmail.com>
2024-03-10 23:06:53 +0300

    update sign up/in

--- main/forms.py
+++ main/forms.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    # first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    # last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Last name', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Passw confirmation', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    crypto_exchange = forms.BooleanField(required=False)
-    foreign_exchange = forms.BooleanField(required=False)
-    bank_account = forms.BooleanField(required=False)
-    money_transfer = forms.BooleanField(required=False)
+    crypto_exchange = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'ms-auto'}), required=False)
+    foreign_exchange = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'ms-auto'}), required=False)
+    bank_account = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'ms-auto'}), required=False)
+    money_transfer = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'ms-auto'}), required=False)
 
     class Meta:
         model = User
